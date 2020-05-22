97dc1d051eb5b29eb95a2b6b0284b22fabb77f77
Glitch (starter-react-template) <none>
2020-05-22 08:52:11 +0000

    ⛰🛳 Checkpoint ./src/App.js:15532837/1151 ./src/index.js:15532837/525

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const LIList1 = React.createElement('li', null, 'Go to market');
+const LIList2 = React.createElement('li', null, 'Buy food');
+const LIList3 = React.createElement('li', null, 'Make dinner');
+const app = React.createElement('ul', null, LIList1, LIList2, LIList3);
+const root = document.getElementById('root');
+ReactDOM.render(app, root);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
